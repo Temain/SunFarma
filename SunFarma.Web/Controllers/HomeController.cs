35201ca640694619b60fb24463811bb5776738ea
temain <asv.artm@gmail.com>
2018-04-16 00:10:37 +0300

    Добавьте файлы проекта.

---
+++ SunFarma.Web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SunFarma.Web.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SelectSupplier()
+        {
+            return View();
+        }
+
+        public ActionResult AddOrder()
+        {
+            return View();
+        }
+
+        public ActionResult OrdersHistory()
+        {
+            return View();
+        }
+
+        public ActionResult OrdersInfo()
+        {
+            return View();
+        }
+
+        public ActionResult Reporting()
+        {
+            return View();
+        }
+    }
+}
---
+++ SunFarma.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SunFarma.Web.MvcApplication" Language="C#" %>
