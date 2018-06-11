23a7556fd17e4d699ad9e82e964f4d3a3d27918d
temain <asv.artm@gmail.com>
2018-06-11 22:00:08 +0300

    Цены

---
+++ SunFarma.Web/Controllers/PricePreestrController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SunFarma.Web.Controllers
+{
+    public class PricePreestrController : Controller
+    {
+        // GET: PricePreestr
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
